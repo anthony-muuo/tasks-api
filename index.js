7163879fd9f08a403315dbfa07e5088646d4e955
muuo <anthonymuuo92@gmail.com>
2025-06-12 16:04:09 +0300

    finished the get all tasks endpoint

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 app.use(express.json());
+
+const client = new PrismaClient();
+
+app.get("/tasks", async (req, res) => {
+  try {
+    const allTasks = await client.tasks.findMany({
+      where: { isDeleted: false },
+    });
+    res.status(200).json(allTasks);
+  } catch (error) {
+    res.status(500).json({ message: "failed to fetch all the tasks" });
+  }
+});
 
 const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
