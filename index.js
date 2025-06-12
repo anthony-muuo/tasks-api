f46bf1881e7f7f2ee7bc6ea3236a35e2fd12f308
muuo <anthonymuuo92@gmail.com>
2025-06-12 19:51:20 +0300

    finished the other links

--- index.js
+++ index.js
@@ -6,14 +6,71 @@ app.use(express.json());
 
 const client = new PrismaClient();
 
-app.get("/tasks", async (req, res) => {
+app.get("/tasks", async (_req, res) => {
   try {
     const allTasks = await client.tasks.findMany({
       where: { isDeleted: false },
     });
     res.status(200).json(allTasks);
   } catch (error) {
-    res.status(500).json({ message: "failed to fetch all the tasks" });
+    res.status(500).json("failed to fetch all the tasks");
+  }
+});
+
+app.post("/tasks", async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const newTask = await client.tasks.create({
+      data: {
+        title,
+        description,
+      },
+    });
+    res.status(201).json(newTask);
+  } catch (error) {
+    res.status(500).json("failed to post to the db");
+  }
+});
+
+app.get("/tasks/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const findSpecificTask = await client.tasks.findUnique({
+      where: { id: +id },
+    });
+    res.status(200).json(findSpecificTask);
+  } catch (error) {
+    res.status(500).json("failed to get this specific task");
+  }
+});
+
+app.patch("/tasks/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description } = req.body;
+    const updateTasks = await client.tasks.update({
+      where: { id: +id },
+      data: {
+        title: title && title,
+        description: description && description,
+      },
+    });
+    res.status(200).json(updateTasks);
+  } catch (error) {
+    res.status(500).json("failed to update this specific task");
+  }
+});
+
+app.patch("/tasks/:id/delete", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const removed = await client.tasks.update({
+      where: { id },
+      data: { isDeleted: true },
+    });
+    res.send(200).json(removed);
+  } catch (error) {
+    res.status(500).json("failed to update this specific task");
   }
 });
 
